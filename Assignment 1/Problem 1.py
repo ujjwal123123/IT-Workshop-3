eab55edf06bc80d7488d7fa765f9cd3626920560
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-08-07 23:22:12 +0530

    Solve problem 1

---
+++ Assignment 1/Problem 1.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def string_into_characters(string: str) -> List[str]:
+    """
+    >>> string_into_characters("abc")
+    ['a', 'b', 'c']
+    """
+    return [char for char in string]
