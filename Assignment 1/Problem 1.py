cfb2c0122728b24631dbcbb80cbff88d7032f692
Ujjwal Goel <ujjwal123123@users.noreply.github.com>
2021-08-07 23:30:36 +0530

    Add a doctest

--- Assignment 1/Problem 1.py
+++ Assignment 1/Problem 1.py
@@ -5,5 +5,7 @@ def string_into_characters(string: str) -> List[str]:
     """
     >>> string_into_characters("abc")
     ['a', 'b', 'c']
+    >>> string_into_characters("")
+    []
     """
     return [char for char in string]
